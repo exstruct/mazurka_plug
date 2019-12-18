db1e0bb2cde2f743b83f905afbe5334174ea97cf
Michael Vanasse <michael@simplecast.com>
2019-12-17 18:08:41 -0700

    point to simplecast deps

--- mix.exs
+++ mix.exs
@@ -2,14 +2,15 @@ defmodule Mazurka.Plug.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :mazurka_plug,
-     description: "Plug integration for Mazurka",
-     version: "0.1.7",
-     elixir: "~> 1.0",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     package: package(),
-     deps: deps()]
+    [
+      app: :mazurka_plug,
+      description: "Plug integration for Mazurka",
+      version: "0.1.7",
+      elixir: "~> 1.0",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
   end
 
   def application do
@@ -17,18 +18,13 @@ defmodule Mazurka.Plug.Mixfile do
   end
 
   defp deps do
-    [{:fugue, ">= 0.1.0"},
-     {:html_builder, "~> 0.1"},
-     {:mazurka, ">= 1.0.0"},
-     {:plug, ">= 0.0.0"},
-     {:poison, ">= 2.2.0"},
-     {:ex_doc, ">= 0.0.0", only: :dev}]
-  end
-
-  defp package do
-    [files: ["lib", "mix.exs", "README*"],
-     maintainers: ["Cameron Bytheway"],
-     licenses: ["MIT"],
-     links: %{"GitHub" => "https://github.com/exstruct/mazurka_plug"}]
+    [
+      {:fugue, ">= 0.1.0"},
+      {:html_builder, github: "simplecastapps/html_builder"},
+      {:mazurka, github: "simplecastapps/mazurka"},
+      {:plug, ">= 0.0.0"},
+      {:poison, ">= 2.2.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
   end
 end
